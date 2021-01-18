7fb4756f12219f7f0b7555a6fe722e16224d9c00
Aman Bisht <bishtaman1111@gmail.com>
2021-01-19 00:02:14 +0530

    Music Player

--- app/src/main/java/com/aman/musicplayer/MainActivity.java
+++ app/src/main/java/com/aman/musicplayer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.aman.musicplayer;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,7 +28,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1;
-
+    ArrayList<MusicFiles> musicFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             , REQUEST_CODE);
         }else{
             Toast.makeText(this, "Permission Granteed", Toast.LENGTH_SHORT).show();
+            musicFiles=getAllAudio(this);
         }
     }
 
@@ -48,9 +54,11 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUEST_CODE){
             if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this, "Permission Granteed", Toast.LENGTH_SHORT).show();
+                getAllAudio(this);
             }else{
                 ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
                         , REQUEST_CODE);
+
             }
         }
     }
@@ -101,5 +109,38 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return titles.get(position);
         }
+    }
+
+    public static ArrayList<MusicFiles> getAllAudio(Context context){
+        ArrayList<MusicFiles> tempAudioList= new ArrayList<>();
+        Uri uri= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] Projection ={
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST,
+
+
+
+
+        };
+        Cursor cursor=context.getContentResolver().query( uri, Projection, null, null,null);
+        if(cursor !=null)
+        {
+            while (cursor.moveToNext()){
+                String album=cursor.getString(0);
+                String title=cursor.getString(1);
+                String duration=cursor.getString(2);
+                String path=cursor.getString(3);
+                String artist=cursor.getString(4);
+
+                MusicFiles musicFiles =new MusicFiles(path,title,artist,album,duration);
+                Log.e("path : "+path,"Alnum :"+ album);
+                tempAudioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return tempAudioList;
     }
 }
